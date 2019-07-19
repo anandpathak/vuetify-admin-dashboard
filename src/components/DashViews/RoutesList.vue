b7b2d88614da67b782bb008e2aa4ca8f5a7f4d52
anand <anandpathak69@gmail.com>
2019-07-20 03:14:21 +0530

    route and proxy crud

--- src/components/DashViews/RoutesList.vue
+++ src/components/DashViews/RoutesList.vue
@@ -1,41 +1,41 @@
 <template>
   <v-container fill-height fluid grid-list-xl>
     <v-layout justify-center wrap>
-      <v-flex md12>
+      <v-flex md12 style="margin-bottom: 48px">
         <material-card color="green" flat full-width title="Route Cluster" text="list of all Route cluster">
           <v-spacer></v-spacer>
           <v-text-field v-model="search" append-icon="search" label="Search" single-line hide-details> -->
            </v-text-field>
-          <v-dialog v-model="dialog" max-width="500px" >
+          <v-dialog v-model="ProxyDialog" max-width="500px" >
             <template v-slot:activator="{ on }">
-              <v-btn color="general" dark class="mb-2" v-on="on" @click="addNew">Add new</v-btn>
+              <v-btn color="general" dark class="mb-2" v-on="on" @click="proxyDialogLoad">Add new</v-btn>
             </template>
 
-            <v-card>
-              <v-card-text>
+            <v-card >
+              <v-card-text >
                 <v-container grid-list-md>
                   <v-layout wrap>
                     <v-flex xs12 sm6 md6>
-                      <v-text-field v-model="editedItem.name" label="Name" />
+                      <v-text-field v-model="proxyItem.name" label="Name" />
                     </v-flex>
                     <v-flex xs12 sm6 md6>
-                      <v-text-field v-model="editedItem.remote_url" label="Remote URL" />
+                      <v-text-field v-model="proxyItem.remote_url" label="Remote URL" />
                     </v-flex>
                     <v-flex xs12 sm6 md6>
-                      <v-select  label="Choose User Credential" :items="dialogUserList" item-text="username" item-value="_id" v-on:change="onUserChange"></v-select>
+                      <v-select  label="Choose User Credential" :items="proxyItem.UserList" item-text="username" item-value="_id" v-on:change="onUserChange"></v-select>
                     </v-flex>
-                    <v-flex xs12 sm6 md12 v-if="dialogCredentialList.length > 0 ">
-                      <v-select  label="Choose Authorization Type" :items="dialogCredentialList" item-text="type" item-value="_id" v-model="editedItem.SelectedCredential"></v-select>
+                    <v-flex xs12 sm6 md12 v-if="showCredentialBox">
+                      <v-select  label="Choose Authorization Type" :items="proxyItem.dialogCredentialList" item-text="type" item-value="_id" v-model="proxyItem.SelectedCredential"></v-select>
                     </v-flex>
                      <v-flex xs12 sm12 md12>
-                      <v-select box  attach multiple label="Policy" :items="dialogPolicyList" item-text="name" item-value="name" v-model="editedItem.policyName" v-on:change="onPolicyChange"></v-select>
+                      <v-select box  attach multiple label="Policy" :items="AllowedPolicyList" item-text="name" item-value="name" v-model="proxyItem.policyName" v-on:change="onPolicyChange"></v-select>
                     </v-flex>
                     <v-flex xs12>
                       <v-textarea
                         outline
                         name="input-7-4"
                         label="Add Policies"
-                        v-model="editedItem.policyMatrix"
+                        v-model="proxyItem.policyMatrix"
                       ></v-textarea>
                     </v-flex>
                   </v-layout>
@@ -44,13 +44,13 @@
 
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="blue darken-1" flat @click="close" >Cancel</v-btn>
-                <v-btn color="blue darken-1" flat @click="save" >Save</v-btn>
+                <v-btn color="blue darken-1" flat @click="closeProxyDialog" >Cancel</v-btn>
+                <v-btn color="blue darken-1" flat @click="saveProxy" >Save</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
 
-          <v-data-table :headers="headers" :items="RouteCluster">
+          <v-data-table :headers="headers" :items="proxyList">
             <template slot="headerCell" slot-scope="{ header }">
               <span class="subheading font-weight-light text--darken-3" v-text="header.text" />
             </template>
@@ -59,35 +59,38 @@
               <td>{{ item.remote_url }}</td>
               <td>{{ item._id }}</td>
               <td class="text-xs-right">
-                <v-icon medium class="mr-2" @click="getItems(item._id)">mdi-arrow-down-drop-circle-outline</v-icon>
-                <v-icon medium @click="editItem(item._id)">edit</v-icon>
+                <v-icon medium class="mr-2" @click="getRoutesByProxyId(item._id,item.name)">mdi-arrow-down-drop-circle-outline</v-icon>
+                <v-icon medium @click="editProxyItem(item,proxyDialogLoad)">edit</v-icon>
               </td>
             </template>
           </v-data-table>
         </material-card>
       </v-flex>
-      <v-flex md12 v-if="selectedRoute.value">
-        <material-card color="blue" flat full-width title="Route List" text="list of Routes">
+      <v-flex md12 v-if="selectedProxyItem.value" style="margin-bottom: 48px">
+        <material-card color="blue" flat full-width :title="'Route List: '+ selectedProxyItem.name" text="list of Routes">
           <v-dialog v-model="RouteDialog" max-width="500px" >
             <template v-slot:activator="{ on }">
-              <v-btn color="general" dark class="mb-2" v-on="on" @click="addRouteIntoCluster">Add new</v-btn>
+              <v-btn color="general" dark class="mb-2" v-on="on" @click="getPolicyList">Add new</v-btn>
             </template>
 
             <v-card>
               <v-card-text>
                 <v-container grid-list-md>
                   <v-layout wrap>
-                    <v-flex xs12 sm6 md6>
-                      <v-text-field v-model="editedRouteItem.base_path" label="Base Path" />
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedRouteItem.name" label="*Name" />
+                    </v-flex>
+                    <v-flex xs12 sm6 md4>
+                      <v-text-field v-model="editedRouteItem.base_path" label="*Base Path" />
                     </v-flex>
                     <v-flex xs12 sm6 md6>
-                      <v-text-field v-model="editedRouteItem.remote_path" label="Remote Path" />
+                      <v-text-field v-model="editedRouteItem.remote_path" label="*Remote Path" />
                     </v-flex>
                     <v-flex xs12 sm6 md6>
-                      <v-text-field v-model="editedRouteItem.method" label="Method" />
+                      <v-text-field v-model="editedRouteItem.method" label="*Method" />
                     </v-flex>
                      <v-flex xs12 sm12 md12>
-                      <v-select box  attach multiple label="Policy" :items="dialogPolicyList" item-text="name" item-value="name" v-model="editedRouteItem.policyName" v-on:change="onPolicyChangeEachRoute"></v-select>
+                      <v-select box  attach multiple label="Policy" :items="AllowedPolicyList" item-text="name" item-value="name" v-model="editedRouteItem.policyName" v-on:change="onPolicyChangeEachRoute"></v-select>
                     </v-flex>
                     <v-flex xs12>
                       <v-textarea
@@ -103,8 +106,8 @@
 
               <v-card-actions>
                 <v-spacer />
-                <v-btn color="blue darken-1" flat @click="closeRouteModel" >Cancel</v-btn>
-                <v-btn color="blue darken-1" flat @click="saveRouteModel" >Save</v-btn>
+                <v-btn color="blue darken-1" flat @click="closeRouteDialog" >Cancel</v-btn>
+                <v-btn color="blue darken-1" flat @click="saveRouteDialog" >Save</v-btn>
               </v-card-actions>
             </v-card>
           </v-dialog>
@@ -116,8 +119,8 @@
 
               <td v-for="(k, i) in RouteHeader" :key="i">{{ item[k.value] }}</td>
               <td class="text-xs-right">
-                <v-icon medium class="mr-2" @click="editItem(props.item)">edit</v-icon>
-                <v-icon medium @click="deleteItem(props.item)">delete</v-icon>
+                <v-icon medium class="mr-2" @click="editRouteItem(item,getPolicyList)">edit</v-icon>
+                <v-icon medium @click="deleteRouteItem(item)">delete</v-icon>
               </td>
             </template>
           </v-data-table>
@@ -132,22 +135,22 @@
 import { constants, pbkdf2 } from 'crypto';
   export default {
     created() {
-      this.$http.get('/routes/query?field={"remote_url":1,"name":1}')
+      this.$http.get('/proxy/query?field={"remote_url":1,"name":1, "credential": 1, "policy": 1}')
         .then(response => {
-          this.RouteCluster = response.data
+          this.proxyList = response.data
         })
         .catch(error => console.log(error))
     },
     methods: {
       onPolicyChange(selectedPolicyName) {
         let tempPolicyItem = [];
-        tempPolicyItem =  (this.editedItem.policyMatrix === undefined || this.editedItem.policyMatrix == '') ? [] :  JSON.parse(this.editedItem.policyMatrix);
+        tempPolicyItem =  (this.proxyItem.policyMatrix === undefined) ? [] :  JSON.parse(this.proxyItem.policyMatrix);
         tempPolicyItem = this._PolicyChangeComputation(selectedPolicyName, tempPolicyItem);
-        this.editedItem.policyMatrix = JSON.stringify(tempPolicyItem, null, '\t');
+        this.proxyItem.policyMatrix = JSON.stringify(tempPolicyItem, null, '\t');
       },
       onPolicyChangeEachRoute(selectedPolicyName) {
         let tempPolicyItem = [];
-        tempPolicyItem = (this.editedRouteItem.policyMatrix === undefined || this.editedRouteItem.policyMatrix == '') ? [] :  JSON.parse(this.editedRouteItem.policyMatrix);
+        tempPolicyItem = (this.editedRouteItem.policyMatrix === undefined ) ? [] :  JSON.parse(this.editedRouteItem.policyMatrix);
         tempPolicyItem = this._PolicyChangeComputation(selectedPolicyName, tempPolicyItem);
         this.editedRouteItem.policyMatrix = JSON.stringify(tempPolicyItem, null, '\t');
       },
@@ -155,7 +158,7 @@ import { constants, pbkdf2 } from 'crypto';
         selectedPolicyName.forEach(policy=> {
           const ispolicyexist = tempPolicyItem.find(elem => elem.name === policy);
           if(!ispolicyexist) {
-            tempPolicyItem.push(this.dialogPolicyList.find(elem => elem.name=== policy));
+            tempPolicyItem.push(this.AllowedPolicyList.find(elem => elem.name=== policy));
           }
         });
         tempPolicyItem =  tempPolicyItem.filter(element => {
@@ -169,31 +172,68 @@ import { constants, pbkdf2 } from 'crypto';
       },
       onUserChange (selectedUserId) {
         this.$http.get(`/credentials/query?filter={"userid":"${selectedUserId}"}&field={}`).then(response => {
-          this.dialogCredentialList = response.data;
+          this.proxyItem.dialogCredentialList = response.data;
+          this.showCredentialBox = true;
         })
         .catch( error => console.log(error));
       },
-      addNew(){
+      proxyDialogLoad(){
+        //get the list of users
         this.$http.get(`/users/query?field={"username":1}`).then(response => {
-          this.dialogUserList = response.data;
+          this.proxyItem.UserList = response.data;
         })
         .catch(error => console.log(error));
-        this.addRouteIntoCluster();
+        this.getPolicyList();
       },
-      addRouteIntoCluster() {
+      getPolicyList() {
          this.$http.get(`/credentials/policy/list`).then( response => {
           console.log('policy',response)
-          this.dialogPolicyList = response.data
+          this.AllowedPolicyList = response.data
         })
         .catch( error => console.log(error));
       },
-      getItems(id) {
-        this.selectedRoute.value= id;
-        this.$http.get(`/routes/query?filter={"_id":"${id}"}&field={"routes":1}`).then(response => {
-          this.RouteList = response.data[0].routes;
+      editProxyItem(item, triggerDialog) {
+        this.proxyItem= {
+          _id : item._id,
+          SelectedCredential : item.credential,
+          name: item.name,
+          remote_url: item.remote_url,
+          policyMatrix: JSON.stringify(item.policy,null, '\t'),
+          state : 'edit'
+        }
+        // trigger the form intial function 
+        triggerDialog();
+        // update the credential value inside dialog form
+        this.$http.get(`/credentials/query?filter={"_id": "${item.credential}"}`).then( response => {
+          this.proxyItem.dialogCredentialList = response.data;
+          this.showCredentialBox = true;
+          this.ProxyDialog = true;
+        })
+        console.log(item, this.proxyItem);
+      },
+      editRouteItem (item, triggerDialog) {
+        console.log(item);
+        this.RouteDialog = true;
+        this.editedRouteItem = {
+          _id: item._id,
+          name: item.name,
+          base_path: item.base_path,
+          remote_path: item.remote_path,
+          method: item.method,
+          policyMatrix: JSON.stringify(item.policy,null, '\t'),
+          state : 'edit'
+        }
+        // load policy options
+        triggerDialog();
+      },
+      getRoutesByProxyId(id,name) {
+        this.selectedProxyItem.value= id;
+        this.selectedProxyItem.name = name
+        this.$http.get(`/routes/query?filter={"proxyId":"${id}"}`).then(response => {
+          this.RouteList = response.data;
           this.RouteHeader = this._generateHeader(Object.keys(this.RouteList[0]));
           console.log(this.RouteHeader, this.RouteList)
-          this.selectedRoute.state = true;
+          this.selectedProxyItem.state = true;
         }).catch(error => console.log(error));
         console.log(id);
       },
@@ -206,50 +246,78 @@ import { constants, pbkdf2 } from 'crypto';
           }
         });
       },
-      closeRouteModel () {
+      closeRouteDialog () {
         this.RouteDialog = false;
+        this.editedRouteItem = {}
       },
-      close () {
-        this.dialog = false
+      closeProxyDialog () {
+        this.ProxyDialog = false
         setTimeout(() => {
-          // this.editedItem = Object.assign({}, this.defaultItem)
+          this.proxyItem = Object.assign({}, this.defaultItem)
+          this.showCredentialBox = false;
           this.editedIndex = 1
+          //this.proxyItem = {}
         }, 300)
       },
-      saveRouteModel () {
+      saveRouteDialog () {
+        let url = '';
         const data = {
+          name: this.editedRouteItem.name,
           base_path : this.editedRouteItem.base_path,
           remote_path: this.editedRouteItem.remote_path,
           method: this.editedRouteItem.method,
-          policy : JSON.parse(this.editedRouteItem.policyMatrix || '[]') 
+          policy : JSON.parse(this.editedRouteItem.policyMatrix || '[]'),
         }
-        this.$http.post('routes/appendRoute/'+this.selectedRoute.value, data ).then( response => {
+        if( this.editedRouteItem.state === 'edit'){
+          url = 'routes/update/'+this.editedRouteItem._id
+        } else {
+          url = 'routes/create';
+          data.proxyId = this.selectedProxyItem.value;
+        }
+        this.$http.post(url, data ).then( response => {
           this.editedRouteItem = {};
-          this.RouteList.push(data);
+          // update list again 
+          this.getRoutesByProxyId(this.selectedProxyItem.value, this.selectedProxyItem.name);
           this.RouteDialog = false
         }).catch(error => console.log(error));
       },
-      save () {
-        console.log(this.editedItem);
+      deleteRouteItem(item){
+        this.$http.delete(`/routes/delete/${item._id}`).then(response => {
+            this.getRoutesByProxyId(this.selectedProxyItem.value, this.selectedProxyItem.name);
+        }).catch( error => console.log(error));
+      },
+      saveProxy () {
+        let url = '';
         const data = {
-          name: this.editedItem.name,
-          policy: JSON.parse(this.editedItem.policyMatrix),
-          remote_url: this.editedItem.remote_url,
-          credential: this.editedItem.SelectedCredential,
-          routes: [],
+          name: this.proxyItem.name,
+          policy: JSON.parse(this.proxyItem.policyMatrix),
+          remote_url: this.proxyItem.remote_url,
+          credential: this.proxyItem.SelectedCredential,
         }
-        this.$http.post('/routes/add', data).then(response => {
-          console.log(response.data);
-          this.editedItem = {};
-          this.RouteCluster.push(response.data);
-          this.dialog = false
+        if (this.proxyItem.state === 'edit') {
+          url = '/proxy/update/'+this.proxyItem._id
+        } else {
+          url  = 'proxy/create';
+        }
+        const self = this;
+        this.$http.post(url, data).then(response => {
+          this.proxyItem = Object.assign({}, this.defaultItem)
+          this.showCredentialBox = false;
+          // replace if id entry is updated
+          const elem = this.proxyList.findIndex(cluster => cluster._id == response.data._id);
+          if(elem >= 0 ){
+            self.proxyList[elem] = Object.assign(self.proxyList[elem], response.data);
+          } else {
+            self.proxyList.push(response.data);
+          }
+          self.ProxyDialog = false
         }).catch(error =>  {
           console.log(error)
         });
       }
     },
     data: () => ({
-      dialog: false,
+      ProxyDialog: false,
       RouteDialog: false,
       search : '',
       headers: [{
@@ -274,40 +342,55 @@ import { constants, pbkdf2 } from 'crypto';
           align: 'right'
         }
       ],
-      RouteCluster: [],
+      proxyList: [],
       selectedRoute: {
         value: '',
         state: false
+      },
+      selectedProxyItem: {
+        value: '',
+        state: false,
+        name: ''
       },
       RouteList: [],
       RouteHeader: [],
 
       editedIndex: -1,
-      editedItem: {
+      proxyItem: {
         name: '',
         remote_url: '',
         SelectedCredential: '',
         policyName: '',
-        policyMatrix: ''      
+        policyMatrix: '[]',
+        dialogCredentialList: [],
+        UserList: [],
+   
       },
+      showCredentialBox : true,
       editedRouteItem: {
-        policyMatrix: '',
+        policyMatrix: '[]',
         base_path: '',
         remote_path: '',
-        method: ''
-
+        method: '',
       },
-      defaultItem: { },
-      dialogUserList: [],
-      dialogCredentialList: [],
-      dialogPolicyList: []
+      defaultItem: { 
+        name: '',
+        remote_url: '',
+        SelectedCredential: '',
+        policyName: '',
+        policyMatrix: '[]',
+        dialogCredentialList: [],     
+        UserList: [],
+
+       },
+      AllowedPolicyList: []
     }),
     watch: {
-      dialog (val) {
-        val || this.close()
+      ProxyDialog (val) {
+        val || this.closeProxyDialog()
       },
       RouteDialog (val) {
-        val || this.close();
+        val || this.closeRouteDialog();
       }
     },
     computed: {
